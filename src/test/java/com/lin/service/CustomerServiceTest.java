ddcbeab480286bb9b861256106eed91a0b47a0b9
lkmc2 <lkmc2@163.com>
2018-09-14 13:12:30 +0800

    添加客户服务单元测试

---
+++ src/test/java/com/lin/service/CustomerServiceTest.java
@@ -0,0 +1,68 @@
+package com.lin.service;
+
+import com.lin.model.Customer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author lkmc2
+ * @date 2018/9/14
+ * @description 客户服务单元测试
+ */
+public class CustomerServiceTest {
+
+    // 客户服务
+    private final CustomerService customerService;
+
+    public CustomerServiceTest() {
+        customerService = new CustomerService();
+    }
+
+    @Test
+    public void getCustomerList() {
+        List<Customer> customerList = customerService.getCustomerList("jack");
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void getCustomer() {
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+
+    }
+
+    @Test
+    public void createCustomer() {
+        HashMap<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("name", "customer100");
+        fieldMap.put("contact", "Jahn");
+        fieldMap.put("telephone", "13456785599");
+
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomer() {
+        long id = 1;
+        HashMap<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("contact", "Eric");
+
+        boolean result = customerService.updateCustomer(id, fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void deleteCustomer() {
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
+    }
+
+}
