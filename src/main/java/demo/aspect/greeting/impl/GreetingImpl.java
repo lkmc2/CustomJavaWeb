8960ddb6315d2e4b0a60daae765263a2d7127cf7
lkmc2 <lkmc2@163.com>
2018-09-15 16:16:15 +0800

    添加抛出增强类

--- src/main/java/demo/aspect/greeting/impl/GreetingImpl.java
+++ src/main/java/demo/aspect/greeting/impl/GreetingImpl.java
@@ -14,6 +14,9 @@ public class GreetingImpl implements Greeting {
     @Override
     public void sayHi(String name) {
         System.out.println("Hi! " + name);
+
+        // 故意抛出异常，看异常信息是否被拦截的到
+        throw new RuntimeException("Error");
     }
 
 }
