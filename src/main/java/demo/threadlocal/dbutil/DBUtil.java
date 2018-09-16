f8a678df3d026628c16ca300e014b4dbe3f9b9dd
lkmc2 <lkmc2@163.com>
2018-09-16 12:01:34 +0800

    使用ThreadLocal解决多线程连接数据库时出现的连接关闭异常

--- src/main/java/demo/threadlocal/dbutil/DBUtil.java
+++ src/main/java/demo/threadlocal/dbutil/DBUtil.java
@@ -16,18 +16,27 @@ public class DBUtil {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "123456";
 
-    // 数据库连接
-    private static Connection conn = null;
+    // 存放数据库连接的局部线程变量（使每个线程都拥有自己的连接）
+    private static ThreadLocal<Connection> connContainer = new ThreadLocal<>();
 
     /**
      * 获取数据库连接
      */
     public static Connection getConnection() {
+        // 获取数据库连接
+        Connection conn = connContainer.get();
+
         try {
-            Class.forName(DRIVER);
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            // 获取的连接为空，重新创建连接
+            if (conn == null) {
+                Class.forName(DRIVER);
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            // 设置连接到线程变量中
+            connContainer.set(conn);
         }
         return conn;
     }
@@ -36,12 +45,18 @@ public class DBUtil {
      * 关闭数据库连接
      */
     public static void closeConnection() {
+        // 获取数据库连接
+        Connection conn = connContainer.get();
+
         try {
             if (conn != null) {
                 conn.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            // 移除数据库连接
+            connContainer.remove();
         }
     }
 
--- src/main/java/demo/threadlocal/dbutil/TestThreadMain.java
+++ src/main/java/demo/threadlocal/dbutil/TestThreadMain.java
@@ -19,7 +19,7 @@ public class TestThreadMain {
             thread.start();
         }
         /*
-            运行结果：（多线程运行时出现连接被关闭的异常）
+            运行结果：（多线程运行正常）
             Thread-0
             Thread-1
             Thread-2
@@ -43,30 +43,13 @@ public class TestThreadMain {
             更新产品成功！
             插入日志成功！
             更新产品成功！
-            更新产品成功！
             插入日志成功！
             更新产品成功！
             插入日志成功！
             更新产品成功！
             插入日志成功！
-com.mysql.jdbc.exceptions.jdbc4.MySQLNonTransientConnectionException: No operations allowed after connection closed.
-	at sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
-	at sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)
-	at sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
-	at java.lang.reflect.Constructor.newInstance(Constructor.java:423)
-	at com.mysql.jdbc.Util.handleNewInstance(Util.java:425)
-	at com.mysql.jdbc.Util.getInstance(Util.java:408)
-	at com.mysql.jdbc.SQLError.createSQLException(SQLError.java:919)
-	at com.mysql.jdbc.SQLError.createSQLException(SQLError.java:898)
-	at com.mysql.jdbc.SQLError.createSQLException(SQLError.java:887)
-	at com.mysql.jdbc.SQLError.createSQLException(SQLError.java:861)
-	at com.mysql.jdbc.ConnectionImpl.throwConnectionClosedException(ConnectionImpl.java:1184)
-	at com.mysql.jdbc.ConnectionImpl.checkClosed(ConnectionImpl.java:1179)
-	at com.mysql.jdbc.ConnectionImpl.prepareStatement(ConnectionImpl.java:4056)
-	at com.mysql.jdbc.ConnectionImpl.prepareStatement(ConnectionImpl.java:4025)
-	at demo.threadlocal.dbutil.service.impl.ProductServiceImpl.insertLog(ProductServiceImpl.java:77)
-	at demo.threadlocal.dbutil.service.impl.ProductServiceImpl.updateProductPrice(ProductServiceImpl.java:36)
-	at demo.threadlocal.dbutil.ClientThread.run(ClientThread.java:24)
+            更新产品成功！
+            插入日志成功！
          */
     }
 
