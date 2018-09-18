81c0c222112503ec0028db845893d0e152fcfe8f
lkmc2 <lkmc2@163.com>
2018-09-18 11:43:26 +0800

    添加使用CXF发布和调用REST服务的例子（通用WebClient客户端，带泛型）

--- src/main/java/demo/rest/client/CXFWebClient.java
+++ src/main/java/demo/rest/client/CXFWebClient.java
@@ -1,8 +1,10 @@
 package demo.rest.client;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import demo.rest.entity.Product;
 import org.apache.cxf.jaxrs.client.WebClient;
 
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,10 +24,10 @@ public class CXFWebClient {
         providerList.add(new JacksonJsonProvider());
 
         // 创建客户端连接并获取产品列表
-        List productList = WebClient.create(baseAddress, providerList)
+        List<Product> productList = WebClient.create(baseAddress, providerList)
                 .path("/products")
                 .accept(MediaType.APPLICATION_JSON)
-                .get(List.class);
+                .get(new GenericType<List<Product>>(){ });
 
         // 遍历产品列表
         productList.forEach(System.out::println);
